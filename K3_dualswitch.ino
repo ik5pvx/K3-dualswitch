2849afb33d3b64911645407f3c424c8f9717feb9
Pierfrancesco Caci <pf@tippete.net>
2016-02-06 18:59:34 +0100

    Band mapping, conversion from number output line. Also some inline documentation of how this should work.

--- K3_dualswitch.ino
+++ K3_dualswitch.ino
@@ -24,11 +24,74 @@
    This way, it would be possible to use this interface also 
    with a single-radio antenna switch without changing the code. 
 
+   Band Mapping on the K3
+   
+   Band mapping depends on the setting of CONFIG:KIO3. When set to NOR
+   the bands are as follows:
+
+   160 m =  1
+    80 m =  2
+	60 m =  0
+    40 m =  3
+    30 m =  4
+    20 m =  5
+    17 m =  6
+    15 m =  7
+    12 m =  8
+    10 m =  9
+     6 m = 10
+	 
+   Transverter bands are all mapped to 0.
+   if CONFING:KIO3 is set to TRN, the band reported on band data lines 
+   corresponds to the transverter band (TRN1 = 1, TRN2 = 2, etc.).
+
+   Since transverter bands are irrelevant for HF operations where the antenna
+   switch operates, CONFIG:KIO3 must be set to NOR for all of this to work.
+
+   NOTE: antenna ports on the switch are numbered 1 to 8. 
+
 */
 
 /* CAMELCASE SUCKS */
 
-#include <stdlib.h>
+//#include <stdlib.h>
+
+
+/************** USER CONFIGURABLE PART ***********/  
+
+// should I decide to put an lcd on this thing
+// give a name to each antenna port. keep it short.
+char* antennaname[] = {
+	"Yagi 6m",   // ant 1
+	"Vert 6m",   // ant 2
+	"",          // ant 3
+	"",          // ant 4
+	"",          // ant 5
+	"",          // ant 6
+	"autotuner", // ant 7
+	"dipolo"     // ant 8
+};
+
+// for each possible band, 1 means the antenna can be tuned to it, 0 it can't	
+int bandtoant[11][8] = {
+	// ant1, ant2, ant3, ant4, ant5, ant6, ant7, ant8
+	{ 0, 0, 0, 0, 0, 0, 0, 0 }, // 160 m
+	{ 0, 0, 0, 0, 0, 0, 1, 0 }, //  80 m
+	{ 0, 0, 0, 0, 0, 0, 1, 0 }, //  60 m
+	{ 0, 0, 0, 0, 0, 0, 1, 1 }, //  40 m
+	{ 0, 0, 0, 0, 0, 0, 1, 0 }, //  30 m
+	{ 0, 0, 0, 0, 0, 0, 1, 1 }, //  20 m
+	{ 0, 0, 0, 0, 0, 0, 1, 0 }, //  17 m
+	{ 0, 0, 0, 0, 0, 0, 1, 1 }, //  15 m
+	{ 0, 0, 0, 0, 0, 0, 1, 0 }, //  12 m
+	{ 0, 0, 0, 0, 0, 0, 1, 1 }, //  10 m
+	{ 1, 1, 0, 0, 0, 0, 0, 0 }, //   6 m
+};
+
+
+/*************************************************/
+
+
 
 /* Hardware setup */
 
@@ -74,14 +137,15 @@ void loop() {
 				}
 			}
 			Serial.println(band);
-			sendbits(band,band);
+			Serial.println(oneofeight(band));
+			sendbits(band,oneofeight(band));
 		} else { 
 			Serial.println("Not a hex digit");
 		}
 	}
 }
 
-
+// Write the data to the shift registers
 void sendbits (byte master, byte slave) {
 	// we don't want the relays to click while we send the data
 	digitalWrite(latchpin, LOW);
@@ -91,4 +155,14 @@ void sendbits (byte master, byte slave) {
 	// data is complete, set latch HIGH and make things change!
 	digitalWrite(latchpin, HIGH);
 
+}
+
+// Number to send to the shift registers to activate the appropriate only
+// output
+int oneofeight (int number) {
+	if ( number ) { 
+		return 1 << number-1;
+	} else {
+		return 0;
+	}
 }
